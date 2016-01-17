d1fc44221cd9f38867e9e53c82ca9135b5e069da
Nikita8877 <nikita8877@mail.ru>
2016-01-17 11:10:35 +0300

    task

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,9 +16,17 @@ class User < ActiveRecord::Base
   validates :uid, uniqueness: { scope: :provider }
 
   def self.from_omniauth(auth_hash)
-    user = User.find_or_initialize_by({uid: auth_hash['uid'], provider: auth_hash['provider']})
-    user.name = auth_hash['info']['name']
-    user.save!
-    user
+    if auth_hash['provider'] == "vkontakte"
+      user = User.find_or_initialize_by({uid: auth_hash['uid'], provider: auth_hash['provider']})
+      user.name = auth_hash['info']['name']
+      user.save!
+      user
+    else
+      user = User.find_or_initialize_by({uid: auth_hash['uid'], provider: auth_hash['provider']})
+      user.name = auth_hash['info']['name']
+      user.email = auth_hash['info']['email']
+      user.save!
+      user
+    end
   end
 end
