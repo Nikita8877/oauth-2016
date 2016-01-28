7c686a19185c6f6c042398c75e2d7fb27968303a
bodrovis <ibodrov@campaigner.ru>
2016-01-28 11:18:37 +0300

    removed table

--- db/migrate/20160120081856_create_appointments.rb
+++ db/migrate/20160120081856_create_appointments.rb
@@ -1,7 +1,6 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t.references :phycisian, index: true, foreign_key: true # :phycisian_id
       t.references :user, index: true, foreign_key: true # :user_id
       t.datetime :scheduled_at
 
